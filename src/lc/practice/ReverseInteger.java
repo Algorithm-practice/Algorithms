89a4323f8a272dc9503d26c1daf2ebcbab05c75f
unknown <chandrasekar_ramasamy@homedepot.com>
2017-08-08 11:24:36 -0400

    reverse int without exception

---
+++ src/lc/practice/ReverseInteger.java
@@ -0,0 +1,22 @@
+package lc.practice;
+
+public class ReverseInteger {
+
+		public static void main(String[] args){
+			ReverseInteger revInt = new ReverseInteger();
+			revInt.reverse(-12300);
+		}
+		public int reverse(int x){
+			int remaining_num = x;
+			int reverseInt = 0;
+			int last_digit;
+			while(remaining_num != 0){
+				last_digit = remaining_num % 10;
+				remaining_num = remaining_num/10;
+				reverseInt = reverseInt*10 + last_digit;
+				System.out.println(reverseInt+"<--------->"+remaining_num);
+			}
+			return reverseInt;
+			
+		}
+}
