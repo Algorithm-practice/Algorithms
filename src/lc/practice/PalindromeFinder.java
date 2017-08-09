0f485f1cde1c0a7f1baa5e96d2e07c4256e4e192
unknown <chandrasekar_ramasamy@homedepot.com>
2017-08-09 09:11:53 -0400

    handled integer overflow

--- src/lc/practice/PalindromeFinder.java
+++ src/lc/practice/PalindromeFinder.java
@@ -3,21 +3,28 @@ package lc.practice;
 public class PalindromeFinder {
 	public static void main (String[] args){
 		PalindromeFinder palindromeFinder =  new PalindromeFinder();
-		palindromeFinder.isPalindrome(-1214);
+		palindromeFinder.isPalindrome(-2147447412);
 	}
 	public boolean isPalindrome(int x) {
-        
+		
+        if(x!= Math.abs(x)){
+        	return false;
+        }
         int remaining_num = x;
 		int reverseInt = 0;
 		int last_digit;
+		try{
 		while(remaining_num != 0){
 			last_digit = remaining_num % 10;
 			remaining_num = remaining_num/10;
-			reverseInt = reverseInt*10 + last_digit;
+			reverseInt = Math.addExact(Math.multiplyExact(reverseInt,10), last_digit);
 		}
 		if (reverseInt == x){
 			return true;
 		}else{
+			return false;
+		}
+		}catch(Exception ex){
 			return false;
 		}
     }
--- src/lc/practice/ReverseInteger.java
+++ src/lc/practice/ReverseInteger.java
@@ -4,19 +4,22 @@ public class ReverseInteger {
 
 		public static void main(String[] args){
 			ReverseInteger revInt = new ReverseInteger();
-			revInt.reverse(-123001);
+			revInt.reverse(Integer.MAX_VALUE );
 		}
 		public int reverse(int x){
 			int remaining_num = x;
 			int reverseInt = 0;
 			int last_digit;
-			while(remaining_num != 0){
-				last_digit = remaining_num % 10;
-				remaining_num = remaining_num/10;
-				reverseInt = reverseInt*10 + last_digit;
-				System.out.println(reverseInt+"<--------->"+remaining_num);
+			try{
+				while(remaining_num != 0){
+					last_digit = remaining_num % 10;
+					remaining_num = remaining_num/10;
+					reverseInt = Math.addExact(Math.multiplyExact(reverseInt,10), last_digit);
+				}
+				return reverseInt;
+			}catch(ArithmeticException  Ex){
+				return 0;
 			}
-			return reverseInt;
 			
 		}
 }
